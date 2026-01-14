69c18523ade20167f2a9f25d240fb4769a50e095
eamonstackpole <113706361+eamonstackpole@users.noreply.github.com>
2026-01-13 19:00:23 -0500

    Update phish_philter.py
    Added very brief layout of the program and how it will function

--- phish_philter.py
+++ phish_philter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import csv # Potentially switch to pandas for larger datasets
 """
 phish_philter.py
 
@@ -6,7 +7,15 @@ phish_philter.py
 
 __version__ = "0.1.0"
 
+# Filter Email Entries (csv) for Phishing Indicators
 def is_phish():
+    pass
+# Export filtered entries to new csv
+def export_phish():
+    pass
+# Print summary statistics
+def print_summary():
 
-
+    pass
 def main():
+    pass
