2f1b87fb30684a39fccc73f7f4bb6ad2d0fa44bf
Dudemantheguy <eamonvonbayern@gmail.com>
2026-01-20 12:02:10 -0500

    Initial reading and filecheck tests

--- phish_philter.py
+++ phish_philter.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-import csv # Potentially switch to pandas for larger datasets
+import openpyxl #necessary for Excel file handling
+import os #used to check file extensions
+import pandas as pd # used for reading data files
+
 """
 phish_philter.py
 
+# Required Libraries: pandas, openpyxl, os
 """
 
 __version__ = "0.1.0"
-
+file_path = 'PhishPhilterTest.csv'
+test_word = 'fig'
 # Filter Email Entries (csv) for Phishing Indicators
 def is_phish():
     pass
@@ -15,7 +20,21 @@ def export_phish():
     pass
 # Print summary statistics
 def print_summary():
-
     pass
+# Check file extension of input file
+def check_extention(expected_extension):
+    file_extension = os.path.splitext(file_path)
+    return file_extension[1] == expected_extension
 def main():
-    pass
+    if check_extention('.csv'):
+        test = pd.read_csv(file_path)
+    elif check_extention('.xlsx'):
+        test = pd.read_excel(file_path)
+    else:
+        print(f"Invalid file extension.")
+        return
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
