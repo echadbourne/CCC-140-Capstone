d9c6b5fe43bfe299b59a9468a227fd86d48350bf
echadbourne <bethchadbourne@gmail.com>
2026-02-17 17:45:05 -0500

    Add pop3 email ps1

---
+++ email.ps1
@@ -0,0 +1,22 @@
+$OpenPopPath = "C:\Users\champuser\OpenPop\OpenPop.dll"
+# Loads dll for use
+[Reflection.Assembly]::LoadFile($OpenPopPath) | Out-Null
+
+$pop3Client = New-Object OpenPop.Pop3.pop3Client
+
+# Variables for setting up the pop3 server
+
+$server = "PhishPhilterPopServer"
+$port = 110
+$enableSSL = $false
+$username = "phishphilteralice@gmail.com"
+$password = "nxgi qixd jxmy eodv"
+
+# Connect to the server
+$pop3Client.connect($server, $port, $enableSSL)
+
+if (!$pop3Client.connected){
+    throw "Unable to connect to POP3 Client. Connection failed with server $server"
+}
+
+$pop3Client.authenticate($username, $password)
